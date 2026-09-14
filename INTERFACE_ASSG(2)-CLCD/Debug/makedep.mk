d2e8967ea5f163ffa10703ede888fd4734d8a0fa
AsaadOmer <iasaadofficial@gmail.com>
2026-09-14 08:35:13 +0200

    Add files via upload

---
+++ INTERFACE_ASSG(2)-CLCD/Debug/makedep.mk
@@ -0,0 +1,12 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+HAL\CLCD\CLCD_prog.c
+
+main.c
+
+MCAL\DIO\dio.c
+
+MCAL\EXTI\EXTI.c
+
